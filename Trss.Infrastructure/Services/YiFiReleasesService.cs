2e37cf5120d3b5365c0d6e8fc64545b3f49664bf
Joao Matos Silva <kappy@acydburne.com.pt>
2015-03-21 08:29:08 +0000

    Changed releases url

--- Trss.Infrastructure/Services/YiFiReleasesService.cs
+++ Trss.Infrastructure/Services/YiFiReleasesService.cs
@@ -14,7 +14,7 @@ namespace Trss.Infrastructure.Services
         {
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("https://yts.re/api/list.json/");
+                client.BaseAddress = new Uri("https://yts.to/api/list.json/");
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
