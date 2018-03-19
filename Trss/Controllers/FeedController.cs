c5cc2e385cc3266d67a5ab2f883b64dc5c75fc75
Joao Matos Silva <kappy@acydburne.com.pt>
2018-03-19 00:48:43 +0000

    Feed

--- Trss/Controllers/FeedController.cs
+++ Trss/Controllers/FeedController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.ServiceModel.Syndication;
+using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Trss.ActionResults;
+using Microsoft.SyndicationFeed;
+using Microsoft.SyndicationFeed.Rss;
 using Trss.Models;
 
 namespace Trss.Controllers
@@ -14,22 +17,43 @@ namespace Trss.Controllers
     public class FeedController : RavenDbBaseController
     {
         // GET: Feed
-        public ActionResult Index(string user)
+        public async Task<ActionResult> Index(string user)
         {
             var items = Session.Query<DownloadRelease>()
                 .Where(t => t.UserId == user)
                 .OrderByDescending(t => t.Date)
                 .Select(BuildSyndicationItem);
 
-            var feed = new SyndicationFeed("Trss Feed", "TTrss Feed", null, items);
-            return new RssActionResult() { Feed = feed };
+            var sw = new StringWriter();
+            using (XmlWriter xmlWriter = XmlWriter.Create(sw, new XmlWriterSettings() {Async = true, Indent = true}))
+            {
+                var writer = new RssFeedWriter(xmlWriter);
+                await writer.WriteTitle("Trss Feed");
+                await writer.WriteDescription("TTrss Feed");
+
+                foreach (var item in items)
+                {
+                    await writer.Write(item);
+                }
+                await writer.Flush();
+            }
+
+            return new ContentResult {Content = sw.ToString(), ContentType = "application/rss+xml"};
         }
 
         private SyndicationItem BuildSyndicationItem(DownloadRelease torrent)
         {
-            var item = new SyndicationItem(torrent.MovieTitleClean, torrent.MovieTitleClean,
-                                           new Uri("https://torcache.net/torrent/" + torrent.TorrentHash + ".torrent"),
-                                           torrent.TorrentHash, torrent.Date);
+            var item = new SyndicationItem()
+            {
+                Title = torrent.MovieTitleClean,
+                Description = torrent.MovieTitleClean,
+                Id = torrent.TorrentHash,
+                Published = torrent.Date
+            };
+            item.AddLink(new SyndicationLink(new Uri("https://torcache.net/torrent/" + torrent.TorrentHash + ".torrent")));
+
+            /*item.
+            
             item.ElementExtensions.Add(
                 new XElement("enclosure",
                              new XAttribute("type", "application/x-bittorrent"),
@@ -37,7 +61,7 @@ namespace Trss.Controllers
                              new XAttribute("url", "https://torcache.net/torrent/" + torrent.TorrentHash + ".torrent")
                     ).CreateReader()
                 );
-
+                */
             return item;
         } 
     }
